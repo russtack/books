b2eae98764863cb2dc5dbaeb34f79e7b43540250
russtack <stirlingest@gmail.com>
2023-04-02 01:21:44 -0400

    new

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,5 +1,36 @@
-function BookShow({ book }) {
-  return <div className="book-show">{book.title}</div>;
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
+function BookShow({ book, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit />;
+  }
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  return (
+    <div className="book-show">
+      <div>{content}</div>
+
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default BookShow;
