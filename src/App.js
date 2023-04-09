337cc0f30d4d1b557071c0d888933cb1b6e379c8
russtack <stirlingest@gmail.com>
2023-04-09 09:28:34 -0400

    axios

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ function App() {
     fetchBooks();
   }, []);
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`);
+    console.log(response);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -29,12 +32,17 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+    console.log(response);
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
         return {
           ...book,
-          title: newTitle,
+          ...response.data,
         };
       }
       return book;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import BooksContext from "./context/books";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BooksContext.Provider value={5}>
+      <App />
+    </BooksContext.Provider>
   </React.StrictMode>
 );
